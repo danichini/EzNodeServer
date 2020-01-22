ef22b1c3858c4915ea41bc459d16c40a3eba532c
Daniel Reverol <daniel.reverol@gmail.com>
2020-01-22 07:15:42 -0500

    dynamic routes first commit

--- app.js
+++ app.js
@@ -2,32 +2,23 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const expressHbs = require('express-handlebars')
+
+const errorController = require('./controllers/error');
 
 const app = express();
-
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
-
-const Controller404 = require('./controllers/404')
-
-
-app.use(bodyParser.urlencoded({
-  extended: false,
-}))
-app.use(express.static(path.join(__dirname, 'public')));
-
-// app.engine('hbs', expressHbs({
-//   layoutsDir: 'views/layouts/',
-//   defaultLayout: 'main-layout',
-//   extname: 'hbs'
-// }));
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
-app.use(Controller404.get404)
 
-app.listen(3000)
+app.use(errorController.get404);
+
+app.listen(3000);
