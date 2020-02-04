d717e89dc8d118763e8a52030f429db9b8a1b20e
Daniel Reverol <daniel.reverol@gmail.com>
2020-02-04 15:36:19 -0500

    create product table from the server using sequelize sync

--- util/database.js
+++ util/database.js
@@ -1,10 +1,7 @@
-const mysql = require('mysql2')
+const { Sequelize } = require('sequelize')
 
-const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  database: 'node_complete'
-  //password: pass
+const sequelize = new Sequelize('node_complete','root','', {
+  dialect: 'mysql', host: 'localhost'
 })
 
-module.exports = pool.promise();
+module.exports = sequelize
