c4d3127b687fcd27ed6791e2c6e5da2b7669f191
Daniel Reverol <daniel.reverol@gmail.com>
2020-02-05 14:51:44 -0500

    fetching all products as admin using sequelize

--- controllers/admin.js
+++ controllers/admin.js
@@ -59,15 +59,17 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
-    res.render('admin/products', {
-      prods: products,
-      pageTitle: 'Admin Products',
-      path: '/admin/products'
-    });
-  });
-};
-
+  Product.findAll()
+    .then(products => {
+      console.log(products);
+      res.render('admin/products', {
+        prods: products,
+        pageTitle: 'Admin Products',
+        path: '/admin/products'
+      });
+    }).catch(err => console.log(err))
+  }
+  
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
   Product.deleteById(prodId);
