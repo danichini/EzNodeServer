f022cf18680a3588c6d90ec5f29032b565fc0c7c
Daniel Reverol <daniel.reverol@gmail.com>
2020-02-10 12:40:35 -0500

    Fetching Related Products

--- controllers/admin.js
+++ controllers/admin.js
@@ -33,8 +33,10 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  Product.findByPk(prodId)
-    .then(product => {
+  req.user.getProducts({where: {id: prodId}})
+  // Product.findByPk(prodId)
+    .then(products => {
+      const product = products[0]
         if (!product) {
             return res.redirect('/');
           }
@@ -72,7 +74,8 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll()
+  req.user.getProducts()
+  // Product.findAll()
     .then(products => {
       console.log(products);
       res.render('admin/products', {
