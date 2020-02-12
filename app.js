6b7dc318dcd319dd3e9f1f4a524b4167b55ddeac
Daniel Reverol <daniel.reverol@gmail.com>
2020-02-12 15:23:19 -0500

    styles updated

--- app.js
+++ app.js
@@ -4,12 +4,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const sequelize = require('./util/database')
-const Product = require('./models/product')
-const User = require('./models/user')
-const Cart = require('./models/cart')
-const CartItem = require('./models/cart-item')
-
+const sequelize = require('./util/database');
+const Product = require('./models/product');
+const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
 
 const app = express();
 
@@ -23,47 +22,46 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  User.findByPk(1)
-  .then(user => {
-    req.user = user;
-    next();
-  })
-  .catch(err => console.log(err))
-})
+  User.findById(1)
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE'});
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
 User.hasMany(Product);
-User.hasOne(Cart)
-Cart.belongsTo(User)
-Cart.belongsToMany(Product, {through: CartItem})
-Product.belongsToMany(Cart, {through: CartItem})
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
 
-//force shouldn't be used in production
 sequelize
-  // .sync({force: true})
+  // .sync({ force: true })
   .sync()
-  .then(res => {
-    return User.findByPk(1)
+  .then(result => {
+    return User.findById(1);
+    // console.log(result);
   })
   .then(user => {
     if (!user) {
-      return User.create({ name: 'Daniel', last: 'Reverol' })
+      return User.create({ name: 'Max', email: 'test@test.com' });
     }
-    return user
+    return user;
   })
   .then(user => {
+    // console.log(user);
     return user.createCart();
   })
   .then(cart => {
-    app.listen(3000)
+    app.listen(3000);
   })
   .catch(err => {
     console.log(err);
-  })
-
-
+  });
