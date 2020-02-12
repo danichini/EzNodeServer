18b37ab49a08bcdc0d978ca490b90987bb3a0a25
Daniel Reverol <daniel.reverol@gmail.com>
2020-02-12 15:38:39 -0500

    fixing db connections bug

--- controllers/shop.js
+++ controllers/shop.js
@@ -85,7 +85,7 @@ exports.postCart = (req, res, next) => {
     res.redirect('/cart')
   })
   .catch(err => console.log(err))
-  // Product.findById(prodId, product => {
+  // Product.findByPk(prodId, product => {
   //   Cart.addProduct(prodId, product.price);
   // });
   // res.redirect('/cart');
@@ -93,7 +93,7 @@ exports.postCart = (req, res, next) => {
 
 exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findById(prodId, product => {
+  Product.findByPk(prodId, product => {
     Cart.deleteProduct(prodId, product.price);
     res.redirect('/cart');
   });
