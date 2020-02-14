6e6541ce99fb9dadd809cdccc746031e71ea5029
Daniel Reverol <daniel.reverol@gmail.com>
2020-02-14 16:09:43 -0500

    first connection to mongodb using mongoAtlas cluster

--- util/database.js
+++ util/database.js
@@ -1,8 +1,13 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient
 
-const sequelize = new Sequelize('node_complete', 'root', '', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+const mongoConnect = (callback) => {
+  MongoClient.connect('mongodb+srv://danichini:7416@cluster0-1lz2a.mongodb.net/test?retryWrites=true&w=majority')
+    .then(client => {
+      console.log('connected')
+      callback(client)
+    })
+    .catch(err => console.log(err))
+}
 
-module.exports = sequelize;
+module.exports = mongoConnect
