256c6d0a99eccfac106bb5a6f1df2c35d613220c
Daniel Reverol <daniel.reverol@gmail.com>
2020-02-28 16:28:14 -0500

    creating users model

--- models/user.js
+++ models/user.js
@@ -1,39 +1,40 @@
-const Sequelize = require('sequelize')
+const mongodb = require("mongodb");
+const getDb = require("../util/database").getDb;
 
-const sequelize = require('../util/database')
+const ObjectId = mongodb.ObjectID
+class User {
+  constructor(username, email) {
+    this.name = username;
+    this.email = email;
+    this._id = id ? mongodb.ObjectId(id) : null;
+  }
 
-const Model = Sequelize.Model;
+  save() {
+    const db = getDb();
+    let dbOp;
+    if (this._id) {
+      dbOp = db.collection("User").updateOne({ _id: this._id }, { $set: this });
+    } else {
+      dbOp = db.collection("User").insertOne(this);
+    }
+    return dbOp
+      .then(result => {
+        console.log("created");
+      })
+      .catch(err => console.log(err));
+  }
 
-class User extends Model {}
-User.init({
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true,
-  },
-  name: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  last: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  // profilePic: {
-  //   type: Sequelize.STRING
-  // },
-  // password: {
-  //   type: Sequelize.STRING,
-  //   allowNull: false
-  // },
-  // email: {
-  //   type: Sequelize.STRING,
-  //   allowNull: false
-  // }
-}, {
-  sequelize,
-  modelName: 'user'
-})
+  static findById(userId) {
+    const db = getDb();
+    return db
+      .collection("users")
+      .findOne({ _id: new mongodb.ObjectId(userId) })
+      .then(result => {
+        console.log("found");
+        return result;
+      })
+      .catch(err => console.log());
+  }
+}
 
-module.exports = User
+module.exports = User;
